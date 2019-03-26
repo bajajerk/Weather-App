8d9b102ca5698d25979c5ae3a362f1aa0cd7941a
bajajerk <bajaj3743@gmail.com>
2019-03-27 03:18:39 +0530

    Create react app, Google places , Autocomp

---
+++ src/components/google-places-search.js
@@ -0,0 +1,59 @@
+import PropTypes from "prop-types"
+import React from "react"
+import ReactGoogleMapLoader from "react-google-maps-loader"
+import ReactGooglePlacesSuggest from "react-google-places-suggest"
+
+const API_KEY = process.env.REACT_APP_GOOGLE_PLACES_API_KEY;
+class GoogleSuggest extends React.Component {
+    state = {
+        search: "",
+        value: "",
+    }
+
+    handleInputChange(e) {
+        this.setState({search: e.target.value, value: e.target.value})
+    }
+
+    handleSelectSuggest(suggest) {
+        console.log(suggest.geometry.location.lat())
+        console.log(suggest.geometry.location.lng())
+
+        this.setState({search: "", value: suggest.formatted_address})
+    }
+
+    render() {
+        const {search, value} = this.state
+        return (
+            <ReactGoogleMapLoader
+                params={{
+                    key: API_KEY,
+                    libraries: "places,geocode",
+                }}
+                render={googleMaps =>
+                    googleMaps && (
+                        <div>
+                            <ReactGooglePlacesSuggest
+                                autocompletionRequest={{input: search}}
+                                googleMaps={googleMaps}
+                                onSelectSuggest={this.handleSelectSuggest.bind(this)}
+                            >
+                                <input
+                                    type="text"
+                                    value={value}
+                                    placeholder="Search a location"
+                                    onChange={this.handleInputChange.bind(this)}
+                                />
+                            </ReactGooglePlacesSuggest>
+                        </div>
+                    )
+                }
+            />
+        )
+    }
+}
+
+GoogleSuggest.propTypes = {
+    googleMaps: PropTypes.object,
+}
+
+export default GoogleSuggest
