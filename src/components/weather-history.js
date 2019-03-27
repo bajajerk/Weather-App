62178ad101857b7a338b41b4881c3a6e94c70397
bajajerk <bajaj3743@gmail.com>
2019-03-28 03:12:31 +0530

    Chart and loader added

--- src/components/weather-history.js
+++ src/components/weather-history.js
@@ -1,11 +1,25 @@
 import React, { Component } from "react";
 import axios from "axios"
+import CircularProgress from '@material-ui/core/CircularProgress';
 import moment from "moment"
+import {
+    LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
+} from 'recharts';
+
+const styles = theme => ({
+    progress: {
+        margin: theme.spacing.unit * 2,
+    },
+});
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            weather_report: '',
+        };
     }
+
     async componentDidMount() {
         var weather_history = [];
         for(var i = 1;i<8;i++)
@@ -14,15 +28,38 @@ class App extends React.Component {
             let resp = await axios.get(
                 `http://api.apixu.com/v1/history.json?key=${process.env.REACT_APP_APIXU_WEATHER_API_KEY}&q=${this.props.latitude},${this.props.longitude}&dt=${date}`
             );
-            weather_history.push(resp.data.forecast.forecastday[0].day)
+            var weather_report = {};
+            // console.log(resp.data.forecast.forecastday[0].day)
+            weather_report.date = date;
+            weather_report.avghumidity = resp.data.forecast.forecastday[0].day.avghumidity;
+            weather_report.avgtemp_c = resp.data.forecast.forecastday[0].day.avgtemp_c;
+            weather_report.totalprecip_mm = resp.data.forecast.forecastday[0].day.totalprecip_mm;
+            weather_history.push(weather_report)
         }
-        console.log(weather_history)
+        await this.setState({weather_report: weather_history})
         // console.log(this.props)
     }
 
     render() {
         return (
-            <p> Hi </p>
+            <div>
+                {!this.state.weather_report &&
+                    <CircularProgress className="loader"/>
+                }
+                {this.state.weather_report &&
+                    <LineChart width={730} height={250} data={this.state.weather_report}
+                               margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="date" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line type="monotone" dataKey="avgtemp_c" stroke="#8884d8" />
+                        <Line type="monotone" dataKey="avghumidity" stroke="#82ca9d" />
+                    </LineChart>
+                }
+            </div>
+
         )
     }
 }
